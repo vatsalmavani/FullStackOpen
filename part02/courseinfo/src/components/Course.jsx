82145606b46fe0c3298b1866cf96f5019427ea41
vatsal <mavanivatsal10@gmail.com>
2024-12-04 14:16:09 +0530

    complete part 2a

---
+++ part02/courseinfo/src/components/Course.jsx
@@ -0,0 +1,40 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <div>
+      <p>
+        Total of {parts.reduce((acc, part) => acc + part.exercises, 0)}{" "}
+        exercises
+      </p>
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
